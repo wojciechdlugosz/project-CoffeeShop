714997310f460004febf6e642e9e6cf107221f19
Wojciech Dlugosz <webmaster@styronet.pl>
2024-07-24 12:16:37 +0200

    Add function changing subpages

---
+++ src/js/app.js
@@ -0,0 +1,61 @@
+import {select, classNames} from './settings.js';
+
+const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        console.log(clickedElement);
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
+  init: function() {
+    const thisApp = this;
+
+    thisApp.initPages();
+  },
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,20 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    home: '.home-wrapper',
+    products: '.products-wrapper',
+    contact: '.contact-wrapper',
+  },
+  nav: {
+    links: '.header__nav a'
+  }
+}; 
+
+export const classNames = {
+  pages: {
+    active: 'active'
+  },
+  nav: {
+    active: 'active'
+  }
+};
