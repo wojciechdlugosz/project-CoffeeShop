aa9a1c288dfd5c2311d404d73de0c1aac4806471
Wojciech Dlugosz <webmaster@styronet.pl>
2024-07-29 10:45:44 +0200

    Add flickity

--- src/js/app.js
+++ src/js/app.js
@@ -1,70 +1,66 @@
 import Contact from './components/Contact.js';
 import Products from './components/Products.js';
-import utils from './utils.js';
 import {select, classNames, settings} from './settings.js';
 
 const app = {
   initPages: function(){
     const thisApp = this;
-
+  
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-
+  
     const idFromHash = window.location.hash.replace('#/', '');
-
+  
     let pageMatchingHash = thisApp.pages[0].id;
-
+  
     for(let page of thisApp.pages){
       if(page.id == idFromHash){
         pageMatchingHash = page.id;
         break;
       }
     }
-
+  
     thisApp.activatePage(pageMatchingHash);
-
+  
     for(let link of thisApp.navLinks){
       link.addEventListener('click', function(event){
         const clickedElement = this;
         event.preventDefault();
-
+  
         const id = clickedElement.getAttribute('href').replace('#', '');
-
+  
         thisApp.activatePage(id);
-
+  
         window.location.hash = '#/' + id;
       });
     }
     thisApp.dynamicTitle();
+    thisApp.carouselWidget();
   },
-
+  
   activatePage: function(pageId){
     const thisApp = this;
-
+  
     for(let page of thisApp.pages){
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
-
+  
     for(let link of thisApp.navLinks){
       link.classList.toggle(
         classNames.nav.active, 
         link.getAttribute('href') == '#' + pageId
       );
     }
-
+  
   },
 
   generateProducts: function(){
     const thisApp = this;
 
-    const productsHeaderHTML = '<h2 class="page__title">products</h2>';
-    const productsHeaderDOM = utils.createDOMFromHTML(productsHeaderHTML);
-    const productsWrapper = document.querySelector(select.containerOf.products);
-    productsWrapper.appendChild(productsHeaderDOM);
-
     for (let productData in thisApp.data.products){
       new Products(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
+
   },
 
   initData: function(){
@@ -84,6 +80,7 @@ const app = {
 
         thisApp.generateProducts();
       });
+
   },
 
   initContact: function(){
@@ -110,12 +107,26 @@ const app = {
 
   },
 
+  carouselWidget: function(){   
+    const carouselWrapper = document.querySelector('.main-carousel');
+
+    // eslint-disable-next-line no-undef
+    const flkty = new Flickity(carouselWrapper, {
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      autoPlay: 5000,
+    });
+    console.log(flkty);
+  },
+  
   init: function() {
     const thisApp = this;
 
     thisApp.initPages();
     thisApp.initContact();
     thisApp.initData();
+
   },
 };
 
