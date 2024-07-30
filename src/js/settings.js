0f61f5bb85a885342e7ab64f02e461aa0e90c2d0
Wojciech Dlugosz <webmaster@styronet.pl>
2024-07-30 17:14:13 +0200

    Prepare for publication

--- src/js/settings.js
+++ src/js/settings.js
@@ -25,7 +25,7 @@ export const classNames = {
 
 export const settings = {
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
   },
 };
